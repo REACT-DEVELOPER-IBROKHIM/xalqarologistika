b3cb9c6bc2cd848878e4fe3135bdfe108ea57364
REACT-DEVELOPER-IBROKHIM <ij.jalalov.ibrokhim@gmail.com>
2025-01-02 19:42:17 +0500

    fix:optimzation fixes

--- client/src/static/CertificateTypeList.jsx
+++ client/src/static/CertificateTypeList.jsx
@@ -1,5 +1,5 @@
 const certificateTypeList = (type, path) => {
-    const endpoints = ["driver", "manager", "adr", "adr-tank"]
+    const endpoints = ['driver', 'manager', 'adr', 'adr-tank']
     return endpoints.map(endpoint => ({
         route: `${path}/${type}-driver-${endpoint}-certificate`,
         name: `${endpoint[0].toUpperCase() + endpoint.slice(1)} certificate`,
