73e929b4bdb805e82366c9c682fa1640f088ff89
REACT-DEVELOPER-IBROKHIM <ij.jalalov.ibrokhim@gmail.com>
2025-02-15 12:58:59 +0500

    feat:added filtering to the tables

--- client/src/routes/sub-routes/manage-certificate/ManageCertificate.jsx
+++ client/src/routes/sub-routes/manage-certificate/ManageCertificate.jsx
@@ -12,16 +12,9 @@ const ManageCertificates = () => {
     setCurrent(e.key);
   };
 
-  const handleDocumentSearch = () => {};
-
   return (
     <div className="flex flex-col">
       <nav className="bg-white flex items-center shadow-3xl mb-4 p-2 px-8">
-        <Search
-          placeholder="ID raqami bo'yicha, ismi yoki familiyasi bo'yicha qidirish"
-          onSearch={handleDocumentSearch}
-          enterButton
-        />
         <Menu
           className="border-none flex gap-4"
           onClick={onClick}
