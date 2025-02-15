684e1911a0c1e43d7aab4a636864f41653dbdb96
REACT-DEVELOPER-IBROKHIM <ij.jalalov.ibrokhim@gmail.com>
2025-02-15 11:36:34 +0500

    fix:birthdate null issue

--- client/src/routes/sub-routes/create/form/index.jsx
+++ client/src/routes/sub-routes/create/form/index.jsx
@@ -26,11 +26,11 @@ const CreateForm = ({ setDocument, document }) => {
       name="basic"
       initialValues={{
         ...document,
-        birthDate: document.birthDate
+        birthDate: document?.birthDate
           ? dayjs(document.birthDate, DATE_FORMAT)
           : null,
-        from: document.from ? dayjs(document.from, DATE_FORMAT) : null,
-        to: document.to ? dayjs(document.to, DATE_FORMAT) : null,
+        from: document?.from ? dayjs(document.from, DATE_FORMAT) : null,
+        to: document?.to ? dayjs(document.to, DATE_FORMAT) : null,
       }}
       layout="vertical"
       onFinish={onFinish}
