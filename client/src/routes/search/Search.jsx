db2a82c58692d5082bdc90da3055a0ab6f2cc961
REACT-DEVELOPER-IBROKHIM <ij.jalalov.ibrokhim@gmail.com>
2025-02-22 19:58:54 +0500

    fix:expired watermark

--- client/src/routes/search/Search.jsx
+++ client/src/routes/search/Search.jsx
@@ -1,7 +1,6 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "./Search.scss";
 import { useParams } from "react-router-dom";
-import Draft from "@routes/draft/Draft";
 import { fetchSingleDocumentByIdThunk } from "@/redux/thunks/single-document-thunk";
 import {
   getSingleDocument,
@@ -11,8 +10,10 @@ import { Spin } from "antd";
 import { useDispatch, useSelector } from "react-redux";
 import DriverCertificate from "@/components/documents/driver";
 import AdrCertificate from "@/components/documents/adr";
+import { Watermark } from "antd";
 
 const Search = () => {
+  const [isCertificateValid, setIsCertificateValid] = useState(null);
   const dispatch = useDispatch();
   const document = useSelector(getSingleDocument);
   const loading = useSelector(getSingleDocumentLoading);
@@ -21,24 +22,62 @@ const Search = () => {
   useEffect(() => {
     dispatch(fetchSingleDocumentByIdThunk(id));
   }, []);
+
+  useEffect(() => {
+    if (document) {
+      const { to } = document;
+      const currentYear = new Date().getFullYear();
+      const currentMonth = new Date().getMonth() + 1;
+      const currentDay = new Date().getDate();
+
+      const year = +to.split(".")[2];
+      const month = +to.split(".")[1];
+      const day = +to.split(".")[0];
+
+      const isCurrentDateValid = () => {
+        if (currentYear > year) return true;
+        if (currentYear === year && currentMonth > month) return true;
+        if (currentYear === year && currentMonth === month && currentDay >= day)
+          return true;
+        return false;
+      };
+
+      setIsCertificateValid(isCurrentDateValid());
+    }
+  }, [document]);
+
   return (
-    <div className="search-certificate">
-      {!loading && document ? (
-        <div className="search-certificate__content">
-          <div className="search-wrapper">
-            {document.id?.startsWith("D") || document.id?.startsWith("M") ? (
-              <DriverCertificate document={document} type={"search"} />
-            ) : (
-              <AdrCertificate document={document} type={"search"} />
-            )}
+    <Watermark
+      content={isCertificateValid ? `EXPIRED` : `NAMANGANTRANS 2022`}
+      gap={[10, 10]}
+      font={{
+        fontSize: 20,
+        color: isCertificateValid
+          ? `rgba(255, 0, 0, 0.5)`
+          : `rgba(0, 0, 0, 0.5)`,
+      }}
+    >
+      <div className="search-certificate">
+        {!loading && document ? (
+          <div className="search-certificate__content">
+            <div className={`search-wrapper`}>
+              <div
+                className={`w-full h-full absolute top-0 left-0 z-10 ${isCertificateValid ? "backdrop-blur-[20px]" : ""}`}
+              ></div>
+              {document.id?.startsWith("D") || document.id?.startsWith("M") ? (
+                <DriverCertificate document={document} type={"search"} />
+              ) : (
+                <AdrCertificate document={document} type={"search"} />
+              )}
+            </div>
           </div>
-        </div>
-      ) : !document && !loading ? (
-        <p>No data</p>
-      ) : (
-        <Spin tip="Loading..." />
-      )}
-    </div>
+        ) : !document && !loading ? (
+          <p>No data</p>
+        ) : (
+          <Spin tip="Loading..." />
+        )}
+      </div>
+    </Watermark>
   );
 };
 
