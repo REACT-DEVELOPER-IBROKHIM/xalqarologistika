48c24274f6ed52d4eb843df70fe5b3ace01604e9
REACT-DEVELOPER-IBROKHIM <ij.jalalov.ibrokhim@gmail.com>
2025-03-01 17:50:16 +0500

    fix:video frame

--- client/src/routes/image-upload/index.jsx
+++ client/src/routes/image-upload/index.jsx
@@ -2,6 +2,7 @@ import { CameraFilled } from "@ant-design/icons";
 import React from "react";
 import Webcam from "react-webcam";
 import axios from "axios";
+import "./index.scss";
 
 const videoConstraints = {
   width: 720,
@@ -28,8 +29,22 @@ const ImageUpload = () => {
       });
       const data = response.data;
       setUrl(data?.microsoft?.image_resource_url);
-    } catch (error) {
-      console.error(error);
+      fetch(data?.microsoft?.image_resource_url)
+      .then(response => response.blob())
+      .then(blob => {
+        const file = new File([blob], "filename.jpg", { type: "image/jpeg" });
+      
+        const formData = new FormData();
+        formData.append('file', file);
+
+        console.log('File:', file);
+      })
+      .catch(error => {
+        console.error('Error fetching and converting image:', error);
+      })
+    }
+    catch (error) {
+      console.error("Error uploading image: ", error);
     }
   };
 
@@ -44,22 +59,24 @@ const ImageUpload = () => {
   };
 
   return (
-    <div className="flex flex-col justify-center items-center relative">
+    <div className="video-container flex max-w-[768px] w-full mx-auto h-screen flex-col justify-center items-center relative">
+      <div className="w-[400px] h-[500px] border-2 border-gray-300 rounded-full absolute top-[100px] z-10"></div>
       <Webcam
         ref={webcamRef}
         height={1280}
         screenshotFormat="image/jpeg"
-        width={720}
         mirrored={true}
         videoConstraints={videoConstraints}
       />
       <button
-        className="absolute bottom-4 p-2 bg-white rounded-md"
+        className="absolute bottom-4 p-[30px] rounded-full bg-white"
         onClick={captureImage}
       >
-        <CameraFilled />
+        <CameraFilled className="text-3xl" />
       </button>
-      {url && <img src={url} alt="" />}
+      {
+      url && <img src={url} alt="" />
+      }
     </div>
   );
 };
