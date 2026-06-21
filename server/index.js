621b3d5cf1888cc9830c58daa4fd298ba5eb5478
REACT-DEVELOPER-IBROKHIM <ij.jalalov.ibrokhim@gmail.com>
2026-06-22 00:59:26 +0500

    fix: remove credentials option from CORS configuration

--- server/index.js
+++ server/index.js
@@ -31,7 +31,6 @@ const corsOptions = {
             callback(new Error('Not allowed by CORS'))
         }
     },
-    credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
 }
