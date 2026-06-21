820a59de09dbf96edaa00b1f38d8c3c7946ab6ad
REACT-DEVELOPER-IBROKHIM <ij.jalalov.ibrokhim@gmail.com>
2026-06-22 00:45:04 +0500

    fix: update CORS middleware to allow requests without origin

--- server/index.js
+++ server/index.js
@@ -26,12 +26,15 @@ const whitelist = [
 app.use(
     cors({
         origin: function (origin, callback) {
-            if (whitelist.indexOf(origin) !== -1) {
+            if (!origin || whitelist.indexOf(origin) !== -1) {
                 callback(null, true)
             } else {
                 callback(new Error('Not allowed by CORS'))
             }
         },
+        credentials: true,
+        methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+        allowedHeaders: ['Content-Type', 'Authorization'],
     })
 )
 
